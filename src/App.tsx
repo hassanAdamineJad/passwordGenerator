c69a479cf656eac127dbe8d4e1415e515dd82a50
hassanAdamineJad <adaminejadh@gmail.com>
2024-07-03 11:16:12 +0300

    feat: Added a Input component with the clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.scss";
+import Input from "./componets/input";
 
 function App() {
-  return <div className="App">Home Page</div>;
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card_item">
+          <Input
+            type="text"
+            value=""
+            name="result"
+            placeholder="placeholder"
+            onChange={(e) => console.log("e", e.target.value)}
+          />
+        </div>
+        <div className="card_item">text</div>
+        <div className="card_item">range</div>
+        <div className="card_item">check box</div>
+        <div className="card_item">btn</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
