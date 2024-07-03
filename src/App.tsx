dec32350754d715cbab0689af30ed00ab5c4880c
hassanAdamineJad <adaminejadh@gmail.com>
2024-07-03 22:16:14 +0300

    feat: Added checkbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import Input from "./componets/Input";
 import RangeSlider from "./componets/RangeSlider";
+import Checkbox from "./componets/Checkbox";
 
 function App() {
+  const [isLowercase, setIsLowercase] = useState(true);
+  const [isUppercase, setIsUppercase] = useState(false);
+  const [isNumber, setIsNumber] = useState(false);
+  const [isSymbol, setIsSymbol] = useState(false);
   return (
     <div className="container">
       <div className="card">
@@ -20,7 +25,38 @@ function App() {
         <div className="card_item">
           <RangeSlider onChange={(e: any) => console.log("eee", e)} />
         </div>
-        <div className="card_item">check box</div>
+        <div className="card_item">
+          <Checkbox
+            label="Include Lowercase"
+            name="lower"
+            value={isLowercase}
+            setValue={setIsLowercase}
+          />
+        </div>
+        <div className="card_item">
+          <Checkbox
+            label="Include Uppercase"
+            name="upper"
+            value={isUppercase}
+            setValue={setIsUppercase}
+          />
+        </div>{" "}
+        <div className="card_item">
+          <Checkbox
+            label="Include Number"
+            name="number"
+            value={isNumber}
+            setValue={setIsNumber}
+          />
+        </div>{" "}
+        <div className="card_item">
+          <Checkbox
+            label="Include Symbol"
+            name="upper"
+            value={isSymbol}
+            setValue={setIsSymbol}
+          />
+        </div>
         <div className="card_item">btn</div>
       </div>
     </div>
